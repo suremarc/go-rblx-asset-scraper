e3f0448cce1921d38362569daae184eb753c121f
suremarc <8771538+suremarc@users.noreply.github.com>
2022-08-07 03:14:54 -0400

    fix service test

--- packages/scraper/sync/client/service_test.go
+++ packages/scraper/sync/client/service_test.go
@@ -20,7 +20,7 @@ func TestClient(t *testing.T) {
 
 	c := NewClient()
 
-	rng, err := ranges.NewRange(101_000, 101_001)
+	rng, err := ranges.NewRange(100_000, 101_000)
 	require.NoError(t, err)
 
 	resp, err := c.Sync(context.TODO(), Request{
